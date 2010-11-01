5a5c97124ee59152e9cae1274a05c40603210062
akimatter <akm2000@gmail.com>
2010-11-01 13:02:25 +0900

    ruby-1.9系でrcovが動かない現象を修正しました。 http://d.hatena.ne.jp/nedate/20101004/1286183882

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,3 +28,17 @@ def assert_hash(expected, actual)
     assert_equal expected[key], actual[key], "unmatch value for #{key.inspect}"
   end
 end
+
+# http://d.hatena.ne.jp/nedate/20101004/1286183882
+if defined?(Rcov)
+  class Rcov::CodeCoverageAnalyzer
+    def update_script_lines__
+      if '1.9'.respond_to?(:force_encoding)
+        SCRIPT_LINES__.each do |k,v|
+          v.each { |src| src.force_encoding('utf-8') }
+        end
+      end
+      @script_lines__ = @script_lines__.merge(SCRIPT_LINES__)
+    end
+  end
+end
